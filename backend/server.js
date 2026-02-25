bfff7ca7347ddbcfc78e73694c69cd967c849d16
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-25 12:01:00 +0530

    chage

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 // import pool from "./db/db.js";
 import cors from "cors";
-import path from "path";
 import appointmentRouter from "../backend/routers/appointmentRouter.js";
 import callRouter from "../backend/routers/call.routes.js";
 import wabhoke from "../backend/routers/wabhoke.routes.js"
@@ -19,15 +18,10 @@ app.use("/api/appointments", appointmentRouter);
 app.use("/api", callRouter);
 app.use("/twilio",wabhoke);
 
-// ✅ ⭐ React build serve
+// ⭐ React build serve (frontend/dist)
 const __dirname = path.resolve();
+const frontendPath = path.join(__dirname, "../frontend/dist");
 
-app.use(express.static(path.join(__dirname, "dist")));
-
-// ⭐ React Router fallback
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
